9f1d9a357d7e53fd80ff9bd11bbacfac16a61f14
Amita C. Patil <patil.amita23@gmail.com>
2021-09-03 11:40:28 +0530

    Simple first series
    first simple * series

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+
 public class Main {
   public static void main () {
     System.out.println("Hello world.");
     
+  }
+  
+  private static starSeries2 (int lineCount) {
+    
+    for (int i=0; i<lineCount; i++) {
+      for (int j=i; j<=i; j++) {
+        System.out.print("* ");
+      }
+      System.out.println("")
+    }
   }
 }
